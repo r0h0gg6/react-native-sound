2d32d8d8fd0f042f5c5f7038cc353ab4501db465
r0h0gg6 <richard.heng0@walmart.com>
2025-07-18 11:05:13 -0700

    Change SoundPackage from turbo to normal react package for autolink

--- android/src/main/java/com/zmxv/RNSound/SoundPackage.kt
+++ android/src/main/java/com/zmxv/RNSound/SoundPackage.kt
@@ -1,13 +1,13 @@
 package com.zmxv.RNSound;
 
-import com.facebook.react.TurboReactPackage
+import com.facebook.react.BaseReactPackage
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.NativeModule
 import com.facebook.react.module.model.ReactModuleInfoProvider
 import com.facebook.react.module.model.ReactModuleInfo
 import java.util.HashMap
 
-class SoundPackage : TurboReactPackage() {
+class SoundPackage : BaseReactPackage() {
   override fun getModule(name: String, reactContext: ReactApplicationContext): NativeModule? {
     return if (name == SoundModule.NAME) {
       SoundModule(reactContext)
